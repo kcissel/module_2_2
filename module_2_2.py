94649e3086fbf0b36992ae212a03728bfabc62ec
kcissel <golovina.nastya02@gmail.com>
2024-06-09 21:29:57 +0500

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,19 @@
+first = int(input('Введите число:'))
+second = int(input('Введите число:'))
+third = int(input('Введите число:'))
+if first == second == third:
+    print(3)
+elif first == second:
+    print(2)
+elif first == third:
+    print(2)
+elif second == third:
+    print(2)
+else:
+    print(0)
+
+
+
+
+
+
